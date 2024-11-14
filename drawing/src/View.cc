25a9cb14b4e0e4d9d7ae3786ea9a24971686ef41
m11115061 <m11115061@mail.ntust.edu.tw>
2024-11-14 13:00:21 +0800

    add directory: drawing

---
+++ drawing/src/View.cc
@@ -0,0 +1,88 @@
+#include "View.h"
+void View::initial(const int& _w, const int& _h,const std::string& name){
+	width  = _w;
+	height = _h;
+	std::string svgName{"./picture/"};
+	svgName+= name;
+	svgName+=".svg";
+	surface = Cairo::SvgSurface::create(svgName, _w, _h);
+	p_context = Cairo::Context::create(surface);
+	//binack ground is ground
+	p_context->set_source_rgba(0.8,0.8,0.8,0.2);
+	p_context->paint();
+
+	lw = 10.0;
+	r =0.0;
+	g=0.0;
+	b=0.0;
+	a=0.0;
+	//scale = 0.0000025;
+	//scale = 1.0;
+	//translatex = -200000000.0;
+	//translatey = 200000000.0;
+}
+void View::setTranslate(const double& _x, const double& _y, const double& _l){
+	//input parameter is the smallest x the biggest y in the design
+	//first set translate space to window's spaceace
+	//calculate scale
+	scale = (height>width) ? width/_l : height/_l;
+	translatex = -1.0*_x*scale;
+	translatey = -1.0*-1.0*_y*scale; //first is flip
+	if(width> height){
+		translatex += (double)(width-height) / (2.0);
+	}
+	else if((width -height) != 0){
+		translatey += (double)(height-width) / (2.0);
+	}
+
+	//std::cout<<translatex<<","<<translatey<<std::endl;
+}
+void View::setLineWidth(const double& _l){
+	lw = _l;
+}
+void View::setRGBA(const double& _r, const double& _g, const double& _b, const double& _a){
+	r = _r;
+	g = _g;
+	b = _b;
+	a = _a;
+}
+void View::drawRectangle(const double& _x, const double& _y, const double& _w, const double& _h){
+	p_context->set_line_width(lw);
+	p_context->set_source_rgba(r,g,b,a);
+	double tx = _x*scale;
+	double ty = _y*(-1.0)*scale;
+	p_context->rectangle(tx+translatex,ty+translatey,_w*scale,_h*scale);
+	p_context->stroke();
+	//p_context->paint();
+	p_context->save();
+}
+void View::drawArc(const double&_x , const double&_y, const double& _r){
+	p_context->set_line_width(lw);
+	p_context->set_source_rgba(r,g,b,a);
+	//std::cout<<_x<<","<<_y<<"("<<translatex<<","<<translatey<<")->        ";
+	double tx = _x*scale;
+	double ty = _y*(-1.0)*scale;
+	//std::cout<<tx<<","<<ty<<"->        ";
+	//std::cout<<tx+translatex<<","<<ty+translatey<<std::endl;
+	p_context->arc(tx+translatex,ty+translatey,_r*scale,0,2*M_PI);
+	p_context->fill();
+	p_context->save();
+}
+void View::drawLine(const double&_sx , const double&_sy, const double& _ex, const double& _ey){
+	p_context->set_line_width(lw);
+	p_context->set_source_rgba(r,g,b,a);
+	//p_context->set_source_rgba(0.8,0.0,0.0,0.5);
+	double tsx = _sx*scale;
+	double tsy = _sy*(-1.0)*scale;
+	double tex = _ex*scale;
+	double tey = _ey*(-1.0)*scale;
+	p_context->move_to(tsx+translatex,tsy+translatey);
+	p_context->line_to(tex+translatex,tey+translatey);
+	p_context->stroke();
+ 	p_context->save();
+	//p_context->paint();
+}
+void View::output(){
+	p_context->show_page();
+	surface->finish();
+}
---
+++ drawing/src/main.d
@@ -0,0 +1,215 @@
+src/main.o: src/main.cc inc/View.h \
+ /usr/include/cairomm-1.0/cairomm/context.h \
+ /usr/include/cairomm-1.0/cairomm/surface.h \
+ /usr/include/sigc++-2.0/sigc++/slot.h \
+ /usr/include/sigc++-2.0/sigc++/functors/slot.h \
+ /usr/include/sigc++-2.0/sigc++/trackable.h \
+ /usr/lib64/sigc++-2.0/include/sigc++config.h \
+ /usr/include/sigc++-2.0/sigc++/visit_each.h \
+ /usr/include/sigc++-2.0/sigc++/type_traits.h \
+ /usr/include/sigc++-2.0/sigc++/adaptors/adaptor_trait.h \
+ /usr/include/sigc++-2.0/sigc++/functors/functor_trait.h \
+ /usr/include/sigc++-2.0/sigc++/functors/ptr_fun.h \
+ /usr/include/sigc++-2.0/sigc++/functors/mem_fun.h \
+ /usr/include/sigc++-2.0/sigc++/limit_reference.h \
+ /usr/include/sigc++-2.0/sigc++/adaptors/deduce_result_type.h \
+ /usr/include/sigc++-2.0/sigc++/functors/slot_base.h \
+ /usr/include/cairomm-1.0/cairomm/enums.h /usr/include/cairo/cairo.h \
+ /usr/include/cairo/cairo-version.h /usr/include/cairo/cairo-features.h \
+ /usr/include/cairo/cairo-deprecated.h /usr/include/cairo/cairo-ft.h \
+ /usr/include/cairo/cairo.h /usr/include/freetype2/ft2build.h \
+ /usr/include/freetype2/freetype/config/ftheader.h \
+ /usr/include/freetype2/freetype/freetype.h \
+ /usr/include/freetype2/freetype/config/ftconfig.h \
+ /usr/include/freetype2/freetype/config/ftconfig-64.h \
+ /usr/include/freetype2/freetype/config/ftoption.h \
+ /usr/include/freetype2/freetype/config/ftstdlib.h \
+ /usr/include/freetype2/freetype/fttypes.h \
+ /usr/include/freetype2/freetype/ftsystem.h \
+ /usr/include/freetype2/freetype/ftimage.h \
+ /usr/include/freetype2/freetype/fterrors.h \
+ /usr/include/freetype2/freetype/ftmoderr.h \
+ /usr/include/freetype2/freetype/fterrdef.h \
+ /usr/include/cairomm-1.0/cairomm/exception.h \
+ /usr/include/cairomm-1.0/cairomm/device.h \
+ /usr/include/cairomm-1.0/cairomm/types.h \
+ /usr/include/cairomm-1.0/cairomm/refptr.h \
+ /usr/include/cairomm-1.0/cairomm/fontoptions.h \
+ /usr/include/cairo/cairo-pdf.h /usr/include/cairo/cairo-ps.h \
+ /usr/include/cairo/cairo-svg.h \
+ /usr/include/cairomm-1.0/cairomm/fontface.h \
+ /usr/include/cairomm-1.0/cairomm/matrix.h \
+ /usr/include/cairomm-1.0/cairomm/pattern.h \
+ /usr/include/cairomm-1.0/cairomm/path.h \
+ /usr/include/cairomm-1.0/cairomm/scaledfont.h \
+ /usr/include/pangomm-1.4/pangomm/fontdescription.h \
+ /usr/include/glibmm-2.4/glibmm/ustring.h \
+ /usr/lib64/glibmm-2.4/include/glibmmconfig.h \
+ /usr/include/glibmm-2.4/glibmm/unicode.h /usr/include/glib-2.0/glib.h \
+ /usr/include/glib-2.0/glib/galloca.h /usr/include/glib-2.0/glib/gtypes.h \
+ /usr/lib64/glib-2.0/include/glibconfig.h \
+ /usr/include/glib-2.0/glib/gmacros.h \
+ /usr/include/glib-2.0/glib/gversionmacros.h \
+ /usr/include/glib-2.0/glib/garray.h \
+ /usr/include/glib-2.0/glib/gasyncqueue.h \
+ /usr/include/glib-2.0/glib/gthread.h \
+ /usr/include/glib-2.0/glib/gatomic.h /usr/include/glib-2.0/glib/gerror.h \
+ /usr/include/glib-2.0/glib/gquark.h /usr/include/glib-2.0/glib/gutils.h \
+ /usr/include/glib-2.0/glib/gbacktrace.h \
+ /usr/include/glib-2.0/glib/gbase64.h \
+ /usr/include/glib-2.0/glib/gbitlock.h \
+ /usr/include/glib-2.0/glib/gbookmarkfile.h \
+ /usr/include/glib-2.0/glib/gbytes.h \
+ /usr/include/glib-2.0/glib/gcharset.h \
+ /usr/include/glib-2.0/glib/gchecksum.h \
+ /usr/include/glib-2.0/glib/gconvert.h \
+ /usr/include/glib-2.0/glib/gdataset.h /usr/include/glib-2.0/glib/gdate.h \
+ /usr/include/glib-2.0/glib/gdatetime.h \
+ /usr/include/glib-2.0/glib/gtimezone.h /usr/include/glib-2.0/glib/gdir.h \
+ /usr/include/glib-2.0/glib/genviron.h \
+ /usr/include/glib-2.0/glib/gfileutils.h \
+ /usr/include/glib-2.0/glib/ggettext.h /usr/include/glib-2.0/glib/ghash.h \
+ /usr/include/glib-2.0/glib/glist.h /usr/include/glib-2.0/glib/gmem.h \
+ /usr/include/glib-2.0/glib/gnode.h /usr/include/glib-2.0/glib/ghmac.h \
+ /usr/include/glib-2.0/glib/gchecksum.h \
+ /usr/include/glib-2.0/glib/ghook.h \
+ /usr/include/glib-2.0/glib/ghostutils.h \
+ /usr/include/glib-2.0/glib/giochannel.h \
+ /usr/include/glib-2.0/glib/gmain.h /usr/include/glib-2.0/glib/gpoll.h \
+ /usr/include/glib-2.0/glib/gslist.h /usr/include/glib-2.0/glib/gstring.h \
+ /usr/include/glib-2.0/glib/gunicode.h \
+ /usr/include/glib-2.0/glib/gkeyfile.h \
+ /usr/include/glib-2.0/glib/gmappedfile.h \
+ /usr/include/glib-2.0/glib/gmarkup.h \
+ /usr/include/glib-2.0/glib/gmessages.h \
+ /usr/include/glib-2.0/glib/gvariant.h \
+ /usr/include/glib-2.0/glib/gvarianttype.h \
+ /usr/include/glib-2.0/glib/goption.h \
+ /usr/include/glib-2.0/glib/gpattern.h \
+ /usr/include/glib-2.0/glib/gprimes.h /usr/include/glib-2.0/glib/gqsort.h \
+ /usr/include/glib-2.0/glib/gqueue.h /usr/include/glib-2.0/glib/grand.h \
+ /usr/include/glib-2.0/glib/gregex.h \
+ /usr/include/glib-2.0/glib/gscanner.h \
+ /usr/include/glib-2.0/glib/gsequence.h \
+ /usr/include/glib-2.0/glib/gshell.h /usr/include/glib-2.0/glib/gslice.h \
+ /usr/include/glib-2.0/glib/gspawn.h \
+ /usr/include/glib-2.0/glib/gstrfuncs.h \
+ /usr/include/glib-2.0/glib/gstringchunk.h \
+ /usr/include/glib-2.0/glib/gtestutils.h \
+ /usr/include/glib-2.0/glib/gthreadpool.h \
+ /usr/include/glib-2.0/glib/gtimer.h \
+ /usr/include/glib-2.0/glib/gtrashstack.h \
+ /usr/include/glib-2.0/glib/gtree.h \
+ /usr/include/glib-2.0/glib/gurifuncs.h \
+ /usr/include/glib-2.0/glib/guuid.h /usr/include/glib-2.0/glib/gversion.h \
+ /usr/include/glib-2.0/glib/deprecated/gallocator.h \
+ /usr/include/glib-2.0/glib/deprecated/gcache.h \
+ /usr/include/glib-2.0/glib/deprecated/gcompletion.h \
+ /usr/include/glib-2.0/glib/deprecated/gmain.h \
+ /usr/include/glib-2.0/glib/deprecated/grel.h \
+ /usr/include/glib-2.0/glib/deprecated/gthread.h \
+ /usr/include/glib-2.0/glib/glib-autocleanups.h \
+ /usr/include/sigc++-2.0/sigc++/sigc++.h \
+ /usr/include/sigc++-2.0/sigc++/signal.h \
+ /usr/include/sigc++-2.0/sigc++/signal_base.h \
+ /usr/include/sigc++-2.0/sigc++/connection.h \
+ /usr/include/sigc++-2.0/sigc++/adaptors/adaptors.h \
+ /usr/include/sigc++-2.0/sigc++/adaptors/bind.h \
+ /usr/include/sigc++-2.0/sigc++/adaptors/bound_argument.h \
+ /usr/include/sigc++-2.0/sigc++/reference_wrapper.h \
+ /usr/include/sigc++-2.0/sigc++/adaptors/bind_return.h \
+ /usr/include/sigc++-2.0/sigc++/adaptors/hide.h \
+ /usr/include/sigc++-2.0/sigc++/adaptors/retype_return.h \
+ /usr/include/sigc++-2.0/sigc++/adaptors/retype.h \
+ /usr/include/sigc++-2.0/sigc++/adaptors/compose.h \
+ /usr/include/sigc++-2.0/sigc++/adaptors/exception_catch.h \
+ /usr/include/sigc++-2.0/sigc++/adaptors/track_obj.h \
+ /usr/include/sigc++-2.0/sigc++/functors/functors.h \
+ /usr/include/glibmm-2.4/glibmm/value.h \
+ /usr/include/glibmm-2.4/glibmm/refptr.h \
+ /usr/include/glibmm-2.4/glibmm/enums.h \
+ /usr/include/glib-2.0/glib-object.h \
+ /usr/include/glib-2.0/gobject/gbinding.h \
+ /usr/include/glib-2.0/gobject/gobject.h \
+ /usr/include/glib-2.0/gobject/gtype.h \
+ /usr/include/glib-2.0/gobject/gvalue.h \
+ /usr/include/glib-2.0/gobject/gparam.h \
+ /usr/include/glib-2.0/gobject/gclosure.h \
+ /usr/include/glib-2.0/gobject/gsignal.h \
+ /usr/include/glib-2.0/gobject/gmarshal.h \
+ /usr/include/glib-2.0/gobject/gboxed.h \
+ /usr/include/glib-2.0/gobject/glib-types.h \
+ /usr/include/glib-2.0/gobject/genums.h \
+ /usr/include/glib-2.0/gobject/gparamspecs.h \
+ /usr/include/glib-2.0/gobject/gsourceclosure.h \
+ /usr/include/glib-2.0/gobject/gtypemodule.h \
+ /usr/include/glib-2.0/gobject/gtypeplugin.h \
+ /usr/include/glib-2.0/gobject/gvaluearray.h \
+ /usr/include/glib-2.0/gobject/gvaluetypes.h \
+ /usr/include/glib-2.0/gobject/gobject-autocleanups.h \
+ /usr/include/glibmm-2.4/glibmm/value_custom.h \
+ /usr/include/glibmm-2.4/glibmm/value_basictypes.h \
+ /usr/include/pango-1.0/pango/pango-font.h \
+ /usr/include/pango-1.0/pango/pango-coverage.h \
+ /usr/include/pango-1.0/pango/pango-version-macros.h \
+ /usr/include/pango-1.0/pango/pango-features.h \
+ /usr/include/pango-1.0/pango/pango-types.h \
+ /usr/include/pango-1.0/pango/pango-gravity.h \
+ /usr/include/pango-1.0/pango/pango-matrix.h \
+ /usr/include/pango-1.0/pango/pango-script.h \
+ /usr/include/pango-1.0/pango/pango-language.h \
+ /usr/include/pango-1.0/pango/pango-bidi-type.h \
+ /usr/include/pangomm-1.4/pangomm/layout.h \
+ /usr/include/glibmm-2.4/glibmm/object.h \
+ /usr/include/glibmm-2.4/glibmm/objectbase.h \
+ /usr/include/glibmm-2.4/glibmm/class.h \
+ /usr/include/glibmm-2.4/glibmm/signalproxy.h \
+ /usr/include/glibmm-2.4/glibmm/signalproxy_connectionnode.h \
+ /usr/include/glibmm-2.4/glibmm/propertyproxy.h \
+ /usr/include/glibmm-2.4/glibmm/propertyproxy_base.h \
+ /usr/include/glibmm-2.4/glibmm/quark.h \
+ /usr/include/glibmm-2.4/glibmm/debug.h \
+ /usr/include/glibmm-2.4/glibmm/wrap.h \
+ /usr/include/glibmm-2.4/glibmm/utility.h \
+ /usr/include/glibmm-2.4/glibmm/containerhandle_shared.h \
+ /usr/include/glibmm-2.4/glibmm/variant.h \
+ /usr/include/glibmm-2.4/glibmm/varianttype.h \
+ /usr/include/glibmm-2.4/glibmm/variantiter.h \
+ /usr/include/glibmm-2.4/glibmm/variantdbusstring.h \
+ /usr/include/glibmm-2.4/glibmm/error.h \
+ /usr/include/glibmm-2.4/glibmm/exception.h \
+ /usr/include/glibmm-2.4/glibmm/variant_basictypes.h \
+ /usr/include/glibmm-2.4/glibmm/slisthandle.h \
+ /usr/include/pangomm-1.4/pangomm/font.h \
+ /usr/include/pangomm-1.4/pangomm/rectangle.h \
+ /usr/include/pangomm-1.4/pangomm/language.h \
+ /usr/include/glibmm-2.4/glibmm/arrayhandle.h \
+ /usr/include/pango-1.0/pango/pango-attributes.h \
+ /usr/include/pangomm-1.4/pangomm/glyph.h \
+ /usr/include/pango-1.0/pango/pango-glyph.h \
+ /usr/include/pango-1.0/pango/pango-item.h \
+ /usr/include/pangomm-1.4/pangomm/fontmetrics.h \
+ /usr/include/pangomm-1.4/pangomm/coverage.h \
+ /usr/include/pangomm-1.4/pangomm/context.h \
+ /usr/include/glibmm-2.4/glibmm/listhandle.h \
+ /usr/include/pangomm-1.4/pangomm/fontset.h \
+ /usr/include/pango-1.0/pango/pango-fontset.h \
+ /usr/include/pangomm-1.4/pangomm/fontmap.h \
+ /usr/include/pangomm-1.4/pangomm/fontfamily.h \
+ /usr/include/pangomm-1.4/pangomm/fontface.h \
+ /usr/include/pango-1.0/pango/pango-fontmap.h \
+ /usr/include/pangomm-1.4/pangomm/item.h \
+ /usr/include/pangomm-1.4/pangomm/attributes.h \
+ /usr/include/pangomm-1.4/pangomm/color.h \
+ /usr/include/pangomm-1.4/pangomm/attrlist.h \
+ /usr/include/pangomm-1.4/pangomm/attriter.h \
+ /usr/include/pangomm-1.4/pangomm/types.h \
+ /usr/include/pango-1.0/pango/pango-context.h \
+ /usr/include/pangomm-1.4/pangomm/tabarray.h \
+ /usr/include/pango-1.0/pango/pango-tabs.h \
+ /usr/include/pango-1.0/pango/pango-break.h \
+ /usr/include/pangomm-1.4/pangomm/layoutline.h \
+ /usr/include/pango-1.0/pango/pango-layout.h \
+ /usr/include/pango-1.0/pango/pango-glyph-item.h \
+ /usr/include/pangomm-1.4/pangomm/layoutiter.h \
+ /usr/include/pangomm-1.4/pangomm/layoutrun.h
